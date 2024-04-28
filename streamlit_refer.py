9894761e609fa20014d82ee02fac3f7ddaef4b03
summnni <114931152+summnni@users.noreply.github.com>
2024-04-28 19:31:27 +0900

    streamlit_refer.py

--- streamlit_refer.py
+++ streamlit_refer.py
@@ -119,8 +119,8 @@ def get_text(docs):
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=900,
-        chunk_overlap=100,
+        chunk_size=250,
+        chunk_overlap=50,
         length_function=tiktoken_len
     )
     chunks = text_splitter.split_documents(text)
